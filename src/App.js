eef1316d0c6b3cf83a32ccff9dd0cc47be9aeb1c
rodrigoricky <rickyrodrigo.ph@gmail.com>
2023-04-09 11:33:36 +0800

    a

--- src/App.js
+++ src/App.js
@@ -6,15 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Home Pages Import
 import DigitalAgency from './pages/DigitalAgency';
-import CreativeAgency from './pages/CreativeAgency';
-import PersonalPortfolio from './pages/PersonalPortfolio';
-import HomeStartup from './pages/HomeStartup';
-import CorporateAgency from './pages/CorporateAgency';
 
 // Blog Import
 import BlogGridView from './pages/BlogGrid';
 import BlogCategory from './pages/Category';
-import BlogArchive from './pages/Archive';
 import BlogDetails from './pages/BlogDetails';
 
 // Service 
@@ -27,13 +22,10 @@ import ProjectDetails from './pages/ProjectDetails';
 
 // Pages 
 import AboutUs from './pages/AboutUs';
-import OurClients from './pages/OurClients';
 import PricingTable from './pages/PricingTable';
 import Contact from './pages/Contact';
 import ErrorPage from './pages/404';
 import ComingSoon from './pages/ComingSoon';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfUse from './pages/TermsOfUse';
 
 // Css Import
 import './assets/scss/app.scss';
@@ -45,12 +37,6 @@ const App = () => {
 		<ScrollToTop>
 			<Routes>
 				<Route path={process.env.PUBLIC_URL + "/"} element={<DigitalAgency />}/>
-
-				<Route path={process.env.PUBLIC_URL + "/digital-agency"} element={<DigitalAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/creative-agency"} element={<CreativeAgency />}/>
-				<Route path={process.env.PUBLIC_URL + "/personal-portfolio"} element={<PersonalPortfolio />}/>
-				<Route path={process.env.PUBLIC_URL + "/home-startup"} element={<HomeStartup />}/>
-				<Route path={process.env.PUBLIC_URL + "/corporate-agency"} element={<CorporateAgency />}/>
 
 				{/* Blogs */}
 				<Route path={process.env.PUBLIC_URL + "/blog-grid/"} element={<BlogGridView />}/>
@@ -76,8 +62,6 @@ const App = () => {
 				<Route path={process.env.PUBLIC_URL + "/contact"} element={<Contact />}/>
 				<Route path={process.env.PUBLIC_URL + "/404"} element={<ErrorPage />}/>
 				<Route path={process.env.PUBLIC_URL + "/coming-soon"} element={<ComingSoon />}/>
-				<Route path={process.env.PUBLIC_URL + "/privacy-policy"} element={<PrivacyPolicy />}/>
-				<Route path={process.env.PUBLIC_URL + "/terms-use"} element={<TermsOfUse />}/>
 
 				<Route path="*" element={<ErrorPage />} />
 
